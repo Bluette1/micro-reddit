7dcaff62e3da4e09382d52e75bb755d37ac1a210
Buyaki <rittahbuyaki@gmail.com>
2020-07-14 17:28:24 +0300

    Did no.5 of validations and no.1 of associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_125413) do
+ActiveRecord::Schema.define(version: 2020_07_14_141215) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
