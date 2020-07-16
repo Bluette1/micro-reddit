80cb90cf1efab4955d669c398e361c582b795b63
Bluette1 <marylene.sawyer@gmail.com>
2020-07-16 11:26:07 +0200

    Add Comment model and the required foreign keys/associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :email, length: { minimum: 10 }, uniqueness: true, null: false
   validates :title, length: { minimum: 2 }, uniqueness: true, null: false
   has_many :posts
+  has_many :comments
 end
